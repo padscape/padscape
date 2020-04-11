8244802ff70abd864d62958997c50e1fb9fdb540
-Bonfire- <40984528+BonfireScratch@users.noreply.github.com>
2020-04-11 13:05:19 +0300

    Update padController.js

--- src/padController.js
+++ src/padController.js
@@ -9,45 +9,46 @@ getPadContents = id => {
 }
 
 saveToDatabase = () => {
-    const http = new XMLHttpRequest();
-    let type = ((editor.emptyResponse && !hasSaved) || !location.hash) ? "POST" : "PUT";
+    let requestType = ((editor.emptyResponse && !hasSaved) || !location.hash) ? "post" : "put";
     let text = $("#src").val().replace(/"/g, '\\"');
 
-    http.open(type, `https://kouritis.ddns.net/code/${(type === "PUT") ? window.location.hash.substring(1) : ""}`, true);
-    http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    http.send(`Code=${(text !== "") ? text : ' '}&Creator=${username}`);
-
-    if (!location.hash) {
-        http.onreadystatechange = () => {
-            if (http.readyState == 4 && http.status == 200) {
-                location.href = `${location.href.split('#')[0]}#${JSON.parse(http.responseText)['id']}`;
+    $.ajax({
+        url: `https://kouritis.ddns.net/code/${(requestType === 'put') ? window.location.hash.substring(1) : ''}`,
+        type: requestType,
+        data: `Code=${(text !== '') ? text : ' '}&Creator=${username}`,
+        contentType: 'application/x-www-form-urlencoded',
+        dataType: 'json',
+        success: data => {
+            if (requestType === 'post') {
+                location.href = `${location.href.split('#')[0]}#${data['id']}`;
                 location.reload();
             }
         }
-    }
+    });
 }
 
 forkCode = () => {
-    const http = new XMLHttpRequest();
-
-    http.open("POST", `https://kouritis.ddns.net/code/`, true);
-    http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    http.send(`Code=${$("#src").val().replace(/"/g, "'")}&Creator=${username}`);
-
-    http.onreadystatechange = () => {
-        if (http.readyState == 4 && http.status == 200) {
-            location.href = `${location.href.split('#')[0]}#${JSON.parse(http.responseText)['id']}`;
+    $.ajax({
+        url: 'https://kouritis.ddns.net/code/',
+        type: 'post',
+        data: `Code=${$("#src").val().replace(/"/g, "'")}&Creator=${username}`,
+        contentType: 'application/x-www-form-urlencoded',
+        success: data => {
+            console.log(data['id'])
+            location.href = `${location.href.split('#')[0]}#${data['id']}`;
             location.reload();
         }
-    }
+    });
 }
 
 deleteCode = () => {
-    const http = new XMLHttpRequest();
-
-    http.open("DELETE", `https://kouritis.ddns.net/code/${window.location.hash.substring(1)}`, true);
-    http.send();
-
-    location.hash = '';
-    location.reload();
+    $.ajax({
+        url: `https://kouritis.ddns.net/code/${window.location.hash.substring(1)}`,
+        type: 'delete',
+        dataType: 'json',
+        success: data => {
+            location.hash = '';
+            location.reload();
+        }
+    });
 }
