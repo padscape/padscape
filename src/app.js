4d95d11e1edfe079c6797ef32ff0ba783e0ea47d
-Bonfire- <40984528+BonfireScratch@users.noreply.github.com>
2020-04-15 12:43:33 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ let defaultText, theme, textSize, realtime, autosave, indentSize, resultShown, s
 let Editor = (() => {
     return {
         init() {
+            // Append the HTML code
             editor = this;
 
             let content = ` <div id="load">
@@ -125,6 +126,8 @@ let Editor = (() => {
 
             $("body").append(content);
 
+            // Get personalized settings from localStorage
+
             isDefined = variable => {
                 return typeof variable !== 'undefined' && variable !== 'undefined';
             }
@@ -139,6 +142,8 @@ let Editor = (() => {
 
             $(':root').css('--indent-size', indentSize);
 
+            // Add the split pane
+
             Split(["#codeCol", "#resultCol"], {
                 elementStyle: (dimension, size, gutterSize) => { 
                     return {'flex-basis': `calc(${size}% - ${gutterSize}px)`};
@@ -148,6 +153,8 @@ let Editor = (() => {
                 gutterSize: 6,
                 cursor: 'col-resize'
             });
+
+            // Get data about the pad
 
             getUsername = pad => {
                 return new Promise((resolve, reject) => {
@@ -205,6 +212,7 @@ let Editor = (() => {
             editor.sizeButtons();
 
             window.onload = () => {
+                // Remove the loading animation
                 $('#load').css('visibility', 'hidden');
                 $('#page').css({'visibility': 'visible', 'opacity': '1'});
             };
@@ -251,6 +259,8 @@ let Editor = (() => {
                 let keyCode = e.keyCode || e.which;
 
                 if (keyCode === 9) {
+                    // When tab key is pressed, increase the identation
+
                     e.preventDefault();
                     let start = this.selectionStart;
                     let end = this.selectionEnd;
@@ -273,6 +283,8 @@ let Editor = (() => {
                     this.selectionStart = 
                     this.selectionEnd = start + 1;
                 } else if (keyCode === 13) {
+                    // When enter key is pressed, preserve the identation
+
                     e.preventDefault();
                     let start = this.selectionStart;
                     let line = this.value.substring(0, this.selectionStart).split('\n').pop();
@@ -316,10 +328,12 @@ let Editor = (() => {
         showResult() {
             let src = '';
 
+            // Fetch the library imports
             for (let lib in libs) { if (libs.hasOwnProperty(lib)) { src += libs[lib]; } }
             src += $('#src').val();
 
-            $('#result')[0].srcdoc = src;
+            // Show the result in the iframe
+            $('#result').attr('srcdoc', src);
         },
         
         renderOutput() {
@@ -334,6 +348,7 @@ let Editor = (() => {
         },
 
         highlight(val) {
+            // Highlight the code using Prism
             $('code', '.code-output').html(val.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;") + "\n");
             Prism.highlightAll();
         },
@@ -345,14 +360,19 @@ let Editor = (() => {
         
         listenerForScroll() {
             $('#src').on('scroll', function() {
-                $('.code-output')[0].scrollTop = this.scrollTop;
-                $('.code-output')[0].scrollLeft = this.scrollLeft;
+                // Scroll the highlighted text to the position of the textarea
+                $('.code-output').scrollTop($(this).scrollTop());
+                $('.code-output').scrollLeft($(this).scrollLeft());
+                // Scroll the textarea to the position of the highlighted text
+                $(this).scrollTop($('.code-output').scrollTop());
+                $(this).scrollLeft($('.code-output').scrollLeft());
             });
         },
 
         sizeButtons() {
             $('.size-plus-btn').click(() => {
-                var current = Number($(':root').css('--text-size').slice(0, -2));
+                // Increase the font size
+                let current = Number($(':root').css('--text-size').slice(0, -2));
 
                 if (current < 55) {
                     $(':root').css('--text-size', `${current + 2}px`);
@@ -363,7 +383,8 @@ let Editor = (() => {
             });
 
             $('.size-minus-btn').click(() => {
-                var current = Number($(':root').css('--text-size').slice(0, -2));
+                // Decrease the font size
+                let current = Number($(':root').css('--text-size').slice(0, -2));
 
                 if (current > 1) {
                     $(':root').css('--text-size', `${current - 2}px`);
@@ -377,36 +398,26 @@ let Editor = (() => {
                 $(':root').css('--text-size', `${textSize}px`);
             });
 
-            $('.size-plus-btn, .size-minus-btn').mouseover(() => {
-                $('.size-plus-btn, .size-minus-btn').animate({opacity: '1'});
-            });
-
-            $('.size-plus-btn, .size-minus-btn').mouseleave(e => {
-                var plusX = Number($('.size-plus-btn').css('left').slice(0, -2));
-                var minusX = Number($('.size-minus-btn').css('left').slice(0, -2));
-
-                var mouseX = e.pageX;
-
-                if (mouseX < minusX || mouseX > plusX) {
-                    $('.size-plus-btn, .size-minus-btn').animate({opacity: '0'});
-                }
-            }); 
-
-            $(document).on('scroll', () => {
-                $('.size-plus-btn, .size-minus-btn').css('bottom', `${$(window).scrollTop() + 26}px`);
-            });
+            position = () => {
+                // Position the element correctly.
+                let position = $('#codeCol').css('width');
+                $('.size-plus-btn').css('left', `calc(${position} - 70px)`);
+                $('.size-minus-btn').css('left', `calc(${position} - 119px)`);
+            }
 
             let observer = new MutationObserver(mutations => {
                 mutations.forEach(() => {
-                    let position = $('#codeCol').css('flex-basis').slice(5, -1);
-                    $('.size-plus-btn').css('left', `calc(${position} - 55px)`);
-                    $('.size-minus-btn').css('left', `calc(${position} - 104px)`);
+                    // When the container is resized
+                    position();
                 });    
             });
             
             observer.observe($('#codeCol')[0], {
                 attributes: true, attributeFilter: ['style']
             });
+
+            // Make position() accessible from the outer scope
+            editor.sizeButtons.position = position;
         },
 
         modal() {
@@ -414,17 +425,19 @@ let Editor = (() => {
 
             $(document).on("keyup keydown", e => {
                 if (e.ctrlKey && e.keyCode === 73) {
+                    // When Ctrl+I pressed
                     e.preventDefault();
                     $("#settingsModal").modal('toggle');
                 }
             });
 
-            $("#settingsModal").on('show.bs.modal', function () {
+            $("#settingsModal").on('show.bs.modal', function() {
                 selectionS = $('#src')[0].selectionStart;
                 selectionE = $('#src')[0].selectionEnd;
             });
 
             $("#settingsModal").on('hidden.bs.modal', () => {
+                // When modal closes, continue where you left off
                 $('#src')[0].selectionStart = selectionS;
                 $('#src')[0].selectionEnd = selectionE;
                 $('#src').focus();
@@ -439,15 +452,17 @@ let Editor = (() => {
             });
 
             $('#darkMode').click(function() {
+                // Change and save the color theme
+
                 if ($(this).is(":checked")) {
                     theme = "dark";
-                    $('#dark')[0].rel = 'stylesheet';
-                    $('#white')[0].rel = 'stylesheet alternate';
+                    $('#dark').attr('rel', 'stylesheet');
+                    $('#white').attr('rel', 'stylesheet alternate');
                     $('.navbar').addClass('bg-dark navbar-dark');
                 } else {
                     theme = "white";
-                    $('#white')[0].rel = 'stylesheet';
-                    $('#dark')[0].rel = 'stylesheet alternate';
+                    $('#white').attr('rel', 'stylesheet');
+                    $('#dark').attr('rel', 'stylesheet alternate');
                     $('.navbar').addClass('bg-light navbar-light');
                 }
 
@@ -455,22 +470,145 @@ let Editor = (() => {
             });
 
             $('#realtimeMode').click(function() {
+                // Change and save the realtime mode
+
                 realtime = ($(this).is(":checked")) ? "on" : "off";
                 localStorage.padscapeRealtime = realtime;
             });
 
             $('#autosaveMode').click(function() {
+                // Change and save the autosave
+
                 autosave = ($(this).is(":checked")) ? "on" : "off";
                 localStorage.padscapeAutosave = autosave;
             });
 
             $('#indentSize').on('change', function() {
-                var indentSize = Number(this.value.slice(0, -7));
+                // Change and save the indent size
+
+                let indentSize = Number(this.value.slice(0, -7));
                 $(':root').css('--indent-size', indentSize);
                 localStorage.padscapeIndentSize = indentSize;
             });
 
+            $('#resultShown').click(() => {
+                resultShown = $('#resultShown').prop('checked');
+                $('#resultCol').css('display', (resultShown) ? 'block' : 'none');
+                localStorage.padscapeResultShown = resultShown;
+                // Position the size buttons
+                editor.sizeButtons();
+                editor.sizeButtons.position();
+            });
+
+            $('.nav-tabs a').click(function() {
+                // Make the tabs in the modal functional
+
+                $('.tab-pane').each((index, element) => { 
+                    $(element).css('display', 'none');
+                });
+                
+                $(this).tab('show');
+                $(`#${this.innerHTML}`).css('display', 'block');
+            });
+
+            $('#libName').on('input keyup', function() {
+                let value = this.value;
+
+                $(this).dropdown('toggle');
+
+                if (value.includes('https:') || value.includes('http:') || value === '' || value === ' ') {
+                    $('.dropdown-menu').removeClass('show');
+                    $('.libsItem').remove();
+                    return;
+                }
+
+                const getLibs = async () => {
+                    // Search the CDN.js API for library results
+                    let response = await fetch(`https://api.cdnjs.com/libraries?search=${value}`);
+                    return await response.json();
+                };
+
+                (async () => {
+                    $('.dropdown-menu')[0].innerHTML = '';
+
+                    let data = await getLibs();
+                    let i = 0;
+
+                    data.results.some((res) => {
+                        if (i > 30 || i >= data.results.length) { return true; }
+                        $('.dropdown-menu').append(`<a class="dropdown-item libsItem" onclick="$('#libName').val($(this)[0].innerHTML); $('.dropdown-menu').removeClass('show'); libLink = '${res.latest}'">${res.name}</a>`);
+                        i++;
+                    });
+                })();
+            });
+
+            $('#addLib').on('click', () => {
+                let libName = $('#libName').val();
+
+                if (libName !== '') {
+                    // Add the library
+
+                    let type = libLink.split('.').pop();
+                    let toAdd = `<${(type === 'js') ? 'script src="' : 'link rel="stylesheet" href="'}${(libName.includes('https://')) ? libName : libLink}">${(type === 'js') ? '</script>' : ''}`;
+                    $('#libList').append(`<li class="list-group-item d-flex align-items-center">${libName} <div id="deleteLib" class="ml-auto"><div class="deleteLib-content">Remove</div></div></li>`);
+                    libs[libName] = toAdd;
+                    localStorage.padscapeLib = JSON.stringify(libs);
+                }
+            });
+
+            $('body').delegate('#deleteLib', 'click', function() {
+                // Remove the library
+
+                let libName = $(this).parent().text().slice(0, -7);
+                delete libs[libName];
+                localStorage.padscapeLib = JSON.stringify(libs);
+                $(this).parent().addClass('deleteLib');
+                setTimeout(() => { $(this).parent().remove(); }, 200);
+            });
+
+            $('#export').tooltip({
+                trigger: 'click',
+                placement: 'bottom'
+            });
+
+            $('#export').on('click', function() {
+                // Copy the complete code to the clipbard
+
+                let index;
+                let src = $('#src').val().split('\n');
+
+                index = src.length;
+                    
+                src.some((line, indx) => {
+                    if (line.includes('</head>')) {
+                        index = indx;
+                        return true;
+                    } else if (line.includes('</body>') || line.includes('</html>')) {
+                        index = indx;
+                    }
+                });
+
+                let targetLine = src[index];
+                let indent = targetLine.match(/^\s*/)[0];
+                if (index === src.length) { lines.push(''); }
+
+                for (let lib in libs) {
+                    if ({}.hasOwnProperty.call(libs, lib)) {
+                        src.splice(index, 0, `${indent}${(indent) ? '\t' : ''}${libs[lib]}`);
+                    }
+                }
+
+                let copy = $('<textarea>').val(src.join('\n')).appendTo('body').select();
+                document.execCommand('copy');
+                copy.remove();
+
+                $(this).tooltip('hide').attr('data-original-title', 'Copied').tooltip('show');
+                setTimeout(() => { $(this).tooltip('hide'); }, 1000);
+            });
+
             $(document).ready(() => {
+                // Set preferences
+
                 if (localStorage.padscapeTheme === "dark") {
                     theme = "dark";
                     $('#dark')[0].rel = 'stylesheet';
@@ -503,109 +641,6 @@ let Editor = (() => {
                         $('#libList').append(`<li class="list-group-item d-flex align-items-center">${lib} <div id="deleteLib" class="ml-auto"><div class="deleteLib-content">Remove</div></div></li>`);
                     }
                 }
-            });
-
-            $('#resultShown').click(() => {
-                resultShown = $('#resultShown').prop('checked');
-                $('#resultCol').css('display', (resultShown) ? 'block' : 'none');
-                localStorage.padscapeResultShown = resultShown;
-            });
-
-            $('.nav-tabs a').click(function() {
-                $('.tab-pane').each((index, element) => { 
-                    $(element).css('display', 'none');
-                });
-                
-                $(this).tab('show');
-                $(`#${this.innerHTML}`).css('display', 'block');
-            });
-
-            $('#libName').on('input keyup', function() {
-                let value = this.value;
-
-                $(this).dropdown('toggle');
-
-                if (value.includes('https:') || value.includes('http:') || value === '' || value === ' ') {
-                    $('.dropdown-menu').removeClass('show');
-                    $('.libsItem').remove();
-                    return;
-                }
-
-                const getLibs = async () => {
-                    let response = await fetch(`https://api.cdnjs.com/libraries?search=${value}`);
-                    return await response.json();
-                };
-
-                (async () => {
-                    $('.dropdown-menu')[0].innerHTML = '';
-
-                    let data = await getLibs();
-                    let i = 0;
-
-                    data.results.some((res) => {
-                        if (i > 30 || i >= data.results.length) { return true; }
-                        $('.dropdown-menu').append(`<a class="dropdown-item libsItem" onclick="$('#libName').val($(this)[0].innerHTML); $('.dropdown-menu').removeClass('show'); libLink = '${res.latest}'">${res.name}</a>`);
-                        i++;
-                    });
-                })();
-            });
-
-            $('#addLib').on('click', () => {
-                let libName = $('#libName').val();
-
-                if (libName !== '') {
-                    let type = libLink.split('.').pop();
-                    let toAdd = `<${(type === 'js') ? 'script src="' : 'link rel="stylesheet" href="'}${(libName.includes('https://')) ? libName : libLink}">${(type === 'js') ? '</script>' : ''}`;
-                    $('#libList').append(`<li class="list-group-item d-flex align-items-center">${libName} <div id="deleteLib" class="ml-auto"><div class="deleteLib-content">Remove</div></div></li>`);
-                    libs[libName] = toAdd;
-                    localStorage.padscapeLib = JSON.stringify(libs);
-                }
-            });
-
-            $('body').delegate('#deleteLib', 'click', function() {
-                let libName = $(this).parent().text().slice(0, -7);
-                delete libs[libName];
-                localStorage.padscapeLib = JSON.stringify(libs);
-                $(this).parent().addClass('deleteLib');
-                setTimeout(() => { $(this).parent().remove(); }, 200);
-            });
-
-            $('#export').tooltip({
-                trigger: 'click',
-                placement: 'bottom'
-            });
-
-            $('#export').on('click', function() {
-                let index;
-                let src = $('#src').val().split('\n');
-
-                index = src.length;
-                    
-                src.some((line, indx) => {
-                    if (line.includes('</head>')) {
-                        index = indx;
-                        return true;
-                    } else if (line.includes('</body>') || line.includes('</html>')) {
-                        index = indx;
-                    }
-                });
-
-                let targetLine = src[index];
-                let indent = targetLine.match(/^\s*/)[0];
-                if (index === src.length) { lines.push(''); }
-
-                for (let lib in libs) {
-                    if ({}.hasOwnProperty.call(libs, lib)) {
-                        src.splice(index, 0, `${indent}${(indent) ? '\t' : ''}${libs[lib]}`);
-                    }
-                }
-
-                let copy = $('<textarea>').val(src.join('\n')).appendTo('body').select();
-                document.execCommand('copy');
-                copy.remove();
-
-                $(this).tooltip('hide').attr('data-original-title', 'Copied').tooltip('show');
-                setTimeout(() => { $(this).tooltip('hide'); }, 1000);
             });
         }
     };
